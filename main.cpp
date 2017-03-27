7375f871992238289ad1a1ca121d84a4e0f97b45
Eric Bitikofer <e.bitikofer@gmail.com>
2017-03-27 19:32:51 -0400

    unicodes constants technique working, but not fully coded

--- main.cpp
+++ main.cpp
@@ -4,4 +4,6 @@ using namespace std;
 
 int main(){
 
+	cout << vd_bar << hd_bar << v_bar << h_bar << bv_bar << bh_bar << endl;
+
 }
--- pieces.h
+++ pieces.h
@@ -1,1 +1,77 @@
 #include <string>
+
+	//FRAME
+		//bars
+		static const std::string h_bar = "\u2500";
+		static const std::string bh_bar = "\u2501";
+		static const std::string v_bar = "\u2502";
+		static const std::string bv_bar = "\u2503";
+		static const std::string hd_bar = "\u2550";
+		static const std::string vd_bar = "\u2551";
+		//single corners
+		static const std::string mr_s_corner = "180";
+		static const std::string tr_s_corner = "191";
+		static const std::string bl_s_corner = "192";
+		static const std::string bm_s_corner = "193";
+		static const std::string tm_s_corner = "194";
+		static const std::string ml_s_corner = "195";
+		static const std::string mm_s_corner = "197";
+		static const std::string br_s_corner = "217";
+		static const std::string tl_s_corner = "218";
+		//double corners
+		static const std::string mr_d_corner = "185";
+		static const std::string tr_d_corner = "187";
+		static const std::string br_d_corner = "188";
+		static const std::string bl_d_corner = "200";
+		static const std::string tl_d_corner = "201";
+		static const std::string bm_d_corner = "202";
+		static const std::string tm_d_corner = "203";
+		static const std::string ml_d_corner = "204";
+		static const std::string mm_d_corner = "206";
+		//double vert corners
+		static const std::string mr_dv_corner = "182";
+		static const std::string tr_dv_corner = "183";
+		static const std::string br_dv_corner = "189";
+		static const std::string ml_dv_corner = "199";
+		static const std::string bm_dv_corner = "208";
+		static const std::string tm_dv_corner = "210";
+		static const std::string bl_dv_corner = "211";
+		static const std::string tl_dv_corner = "214";
+		static const std::string mm_dv_corner = "215";
+		//double hor corners
+		static const std::string mr_dh_corner = "181";
+		static const std::string tr_dh_corner = "184";
+		static const std::string br_dh_corner = "190";
+		static const std::string ml_dh_corner = "198";
+		static const std::string bm_dh_corner = "207";
+		static const std::string tm_dh_corner = "209";
+		static const std::string bl_dh_corner = "212";
+		static const std::string tl_dh_corner = "213";
+		static const std::string mm_dh_corner = "216";
+	//SLIDERS
+		//arrows
+		static const std::string ld_arrow = "174";
+		static const std::string rd_arrow = "175";
+		//sliders
+		static const std::string h_slider = "233";
+		static const std::string v_slider_1 = "232";
+		static const std::string v_slider_2 = "237";
+	//MISC
+		//blocks
+		static const std::string upper_half = "223";
+		static const std::string lower_half = "220";
+		static const std::string left_side = "221";
+		static const std::string right_side = "222";
+		static const std::string square = "254";
+		//special corners
+		static const std::string ls_sh_corner = "169";
+		static const std::string rs_sh_corner = "170";
+		//misc
+		static const std::string degree = "167";
+		static const std::string arc = "239";
+		static const std::string triple_barrel = "240";
+	//ANIMATIONS
+		//fade
+		static const std::string fade_1 = "176";
+		static const std::string fade_2 = "177";
+		static const std::string fade_3 = "178";
